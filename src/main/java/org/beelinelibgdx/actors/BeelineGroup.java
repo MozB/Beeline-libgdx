cbba56474828c90ca1d0cadc14988fd83c78ef4c
MozB <mozb87@gmail.com>
2018-08-26 10:32:39 +0100

    making refresh not abstract

--- src/main/java/org/beelinelibgdx/actors/BeelineGroup.java
+++ src/main/java/org/beelinelibgdx/actors/BeelineGroup.java
@@ -3,13 +3,15 @@ package org.beelinelibgdx.actors;
 import com.badlogic.gdx.scenes.scene2d.Group;
 
 
-public abstract class BeelineGroup extends Group implements BeelineRemovable {
+public class BeelineGroup extends Group implements BeelineRemovable {
 
     public BeelineGroup() {
         setTransform(false);
     }
 
     @Override
-    public abstract void refresh();
+    public void refresh() {
+
+    }
 
 }
