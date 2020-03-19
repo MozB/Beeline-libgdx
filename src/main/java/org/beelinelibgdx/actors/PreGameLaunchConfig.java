0660efe21460a1b725ad57ae4d94f149cc86bcd8
MozB <mozb87@gmail.com>
2020-03-18 23:09:32 -0500

    tweaking config

--- src/main/java/org/beelinelibgdx/actors/PreGameLaunchConfig.java
+++ src/main/java/org/beelinelibgdx/actors/PreGameLaunchConfig.java
@@ -3,10 +3,10 @@ package org.beelinelibgdx.actors;
 public class PreGameLaunchConfig {
 
     public boolean shouldAttemptToGenerateFont = true;
-    public String fontSourceLocalFilePath = "../beeline-in/fonts/font.ttf";
+    public String fontSourceLocalFilePath = "../../beeline-in/fonts/font.ttf";
     public String fontDataOutputFilePath = "beeline-out/fonts/font";
     public boolean shouldAttemptToGenerateSpriteSheet = true;
-    public String spriteSheetSourceLocalDirectoryPath = "../beeline-in/img/";
+    public String spriteSheetSourceLocalDirectoryPath = "../../beeline-in/img/";
     public String spriteSheetOutputLocalDirectoryPath = "beeline-out/img/";
     public String saveGameDirectoryPath = "beeline-out/savegames/";
 
