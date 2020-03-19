4d6f2a23b6e98be91423fdb94f1fd3811bfad8b9
MozB <mozb87@gmail.com>
2020-03-18 22:13:11 -0500

    updating default config paths

--- src/main/java/org/beelinelibgdx/actors/PreGameLaunchConfig.java
+++ src/main/java/org/beelinelibgdx/actors/PreGameLaunchConfig.java
@@ -3,11 +3,11 @@ package org.beelinelibgdx.actors;
 public class PreGameLaunchConfig {
 
     public boolean shouldAttemptToGenerateFont = true;
-    public String fontSourceLocalFilePath = "beeline-in/fonts/font.ttf";
-    public String fontDataOutputFilePath = "src/main/resources/beeline-out/fonts/font";
+    public String fontSourceLocalFilePath = "../beeline-in/fonts/font.ttf";
+    public String fontDataOutputFilePath = "beeline-out/fonts/font";
     public boolean shouldAttemptToGenerateSpriteSheet = true;
-    public String spriteSheetSourceLocalDirectoryPath = "beeline-in/img/";
-    public String spriteSheetOutputLocalDirectoryPath = "src/main/resources/beeline-out/img/";
-    public String saveGameDirectoryPath = "src/main/resources/beeline-out/savegames/";
+    public String spriteSheetSourceLocalDirectoryPath = "../beeline-in/img/";
+    public String spriteSheetOutputLocalDirectoryPath = "beeline-out/img/";
+    public String saveGameDirectoryPath = "beeline-out/savegames/";
 
 }
